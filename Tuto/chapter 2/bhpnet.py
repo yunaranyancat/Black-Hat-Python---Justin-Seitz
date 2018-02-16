e34327c6ccebca44442ebc6c68e05ae510c190be
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-02-16 19:32:31 +0100

    defining usage() function

--- Tuto/chapter 2/bhpnet.py
+++ Tuto/chapter 2/bhpnet.py
@@ -11,3 +11,19 @@ execute = ""
 target = ""
 upload_destination = ""
 port = 0
+
+def usage():
+    print "BHP Net Tool"
+    print
+    print "Usage: bhpnet.py -t target_host -p port"
+    print "-l --listen              - listen on [host]:[port] for incoming connections"
+    print "-e --execute=file_to_run - execute the given file upon receiving a connection"
+    print "-c --command             - initialize a command shell"
+    print
+    print
+    print "Examples: "
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
