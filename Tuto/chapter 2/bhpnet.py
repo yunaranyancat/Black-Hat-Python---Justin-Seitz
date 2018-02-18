73b5a745bc8a6ea7c243c02ca7904030be776724
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-02-18 17:29:50 +0100

    added server_loop() function

--- Tuto/chapter 2/bhpnet.py
+++ Tuto/chapter 2/bhpnet.py
@@ -124,5 +124,24 @@ def client_sender(buffer):
         #tear down the connection
         client.close()
 
+def server_loop():
+    global target
+
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target,port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = sever.accept()
+
+        #spin off a thread to handle our new client
+        client_thread = threading.Thread(target=client_handle,args=(client_socket,))
+        client_thread.start()
+
 
 main()
